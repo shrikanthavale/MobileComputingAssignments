1dad7072571223e50e8e0296fb03ece9a6666fc1
Shrikant <shrikant.havale@yahoo.com>
2014-10-19 18:09:50 +0200

    german properties file

--- SystemEngineering3-MetricTesting/SQE01-Havale_Triangle1st/src/at/fhhagenberg/sqe/exercise1/Messages.java
+++ SystemEngineering3-MetricTesting/SQE01-Havale_Triangle1st/src/at/fhhagenberg/sqe/exercise1/Messages.java
@@ -3,11 +3,22 @@ package at.fhhagenberg.sqe.exercise1;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
+/**
+ * Message - Auto generated using eclipse externalize string for reading properties file
+ * based on locale selected.
+ * 
+ * @author Shrikant Havale - S1310455005
+ * Oct 19, 2014
+ *
+ */
 public class Messages {
 	private static final String BUNDLE_NAME = "at.fhhagenberg.sqe.exercise1.messages"; //$NON-NLS-1$
 
 	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle
 			.getBundle(BUNDLE_NAME);
+	
+//	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle
+//			.getBundle(BUNDLE_NAME, new Locale("DE"));
 
 	private Messages() {
 	}
