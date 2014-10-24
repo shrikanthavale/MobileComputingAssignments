bc45533a0701902c12a9a29e065dde54371262a1
Shrikant <shrikant.havale@yahoo.com>
2014-10-24 13:42:40 +0200

    System Engineering 3 - Metric & Testing - Assignment 3 - Triangle Model Test Project

---
+++ SystemEngineering3-MetricTesting/SQE03-Havale_Triangle1stTest/src/at/fhhagenberg/sqe/exercise3/TriangleTest.java
@@ -0,0 +1,81 @@
+/**
+ * 
+ */
+package at.fhhagenberg.sqe.exercise3;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author mypersonalpc
+ * Oct 24, 2014
+ * 
+ */
+public class TriangleTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link at.fhhagenberg.sqe.exercise3.model.Triangle#calculateArea(double, double, double)}.
+	 */
+	@Test
+	public final void testCalculateArea() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link at.fhhagenberg.sqe.exercise3.model.Triangle#calculatePerimeter(double, double, double)}.
+	 */
+	@Test
+	public final void testCalculatePerimeter() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link at.fhhagenberg.sqe.exercise3.model.Triangle#validateSingleSide(java.lang.String)}.
+	 */
+	@Test
+	public final void testValidateSingleSide() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link at.fhhagenberg.sqe.exercise3.model.Triangle#validateSummationRule3Sides(double, double, double)}.
+	 */
+	@Test
+	public final void testValidateSummationRule3Sides() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
